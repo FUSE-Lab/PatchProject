67dea60c00523dfdad40e5dcd3829c1e71923d3f
Lindsay <ledarling@gmail.com>
2021-05-27 09:02:40 -0400

    Added edge plots and completed pivot to plot, species and BA. Commented bins out but may need to add in later.

--- PatchComp.R
+++ PatchComp.R
@@ -30,7 +30,6 @@ trees <- read_csv('RegionAllTrees2020.csv') %>% #Throws a warning but looks okay
          'TreeID' = 'Tree ID') %>% 
   mutate(dbh_cm = DBH_in*2.54)
 
-
 #Look at each group of trees
 
 #Novel
@@ -49,10 +48,70 @@ regrowthTree<-left_join(regrowth,trees, by = 'PlotID')%>%
 
 merged<-rbind(remnantTree, regrowthTree, novelTree) 
 
-df<-merged[,-c(1:4,6:25,27:28,31)] 
-colnames(df)
-#Do the same thing for plots that are on the edge
+#Drop extra columns
+df<-merged[,-c(1:4,6:29,31:41)] 
 
+#Not using this binning code for now, but may need later.
+
+# #Create DBH bins ----------------
+# #This binning script was written by Dexter Locke for the SESYNC urban
+# #woodlands pursuit. 
+# 
+# #create a column with dbh classes
+# 
+# plots_trees <-  df %>% mutate(dbh_class = santoku::chop(dbh_cm,  
+#                                                         breaks = seq(from = 2.54, #min DBH in dataset
+#                                                                      to = 136, #Max DBH in dataset
+#                                                                      by = 10)),         # 10 cm intervals
+#                               dbh_class = factor(dbh_class, ordered = TRUE), #Put them in order
+#                               ba_m = (dbh_cm^2)*0.00007854) #Add BA
+# View(plots_trees)
+# # how did the dbh class work out?
+# table(plots_trees$dbh_class)
+# barplot(table(plots_trees$dbh_class), las=2) #DBH distribution by class. Boom.
+# 
+# #Now, create a table with 
+# 
+# binned<-plots_trees %>% 
+#   mutate(dbh_class = factor(dbh_class, ordered = TRUE,
+#                             levels = c(
+#                               '[2.54, 12.54)',
+#                               '[12.54, 22.54)',
+#                               '[22.54, 32.54)',
+#                               '[32.54, 42.54)',
+#                               '[42.54, 52.54)',
+#                               '[52.54, 62.54)',
+#                               '[62.54, 72.54)',
+#                               '[72.54, 82.54)', 
+#                               '[82.54, 92.54)',
+#                               '[92.54, 102.5)', 
+#                               '[102.54, 112.5)', 
+#                               '[112.54, 122.5)', 
+#                               '[122.54, 132.5)',
+#                               '[132.54, 134.1'))) %>% 
+#   drop_na(dbh_class) 
+# 
+# View(binned)
+
+#Create a table with plots as rows, species as columns, and sum of BA in cells
+#TODO: this is not working.
+
+df %>% 
+  mutate(ba_m = (dbh_cm^2)*0.00007854) %>% #Add BA
+  st_drop_geometry(.) %>% #pivot_wider doesn't like spatial data
+  select(PlotID, patchType, Species, ba_m) %>% #Only the necessary columns
+  pivot_wider(id_cols = c(PlotID, patchType), #Summarize by PlotID and keep patch type
+              names_from = Species, #Makes species columns
+              names_sort = TRUE, #Sort alphabetically
+              values_from = ba_m, #Cells are BA
+              values_fn = sum, #Add BA together
+              values_fill = 0) -> plotSpeciesBA #Fill NA with 0 and write it
+
+View(plotSpeciesBA) #Looks great!
+
+write_csv(plotSpeciesBA, 'coreSpeciesBA.csv')
+
+#Do the same thing for plots that are on the edge
 
 #Load plot types
 
@@ -78,64 +137,21 @@ regrowthEdgeTree<-left_join(regrowthEdge,trees, by = 'PlotID')%>%
 
 merged<-rbind(remnantEdgeTree, regrowthEdgeTree, novelEdgeTree) 
 
-df<-merged[,-c(1,7:10,27:28,31)] 
+df<-merged[,-c(1,3:5,7:17)] 
 
 summary(df)
 
+df %>% 
+  mutate(ba_m = (dbh_cm^2)*0.00007854) %>% #Add BA
+  st_drop_geometry(.) %>% #pivot_wider doesn't like spatial data
+  select(PlotID, patchType, Species, ba_m) %>% #Only the necessary columns
+  pivot_wider(id_cols = c(PlotID, patchType), #Summarize by PlotID and keep patch type
+              names_from = Species, #Makes species columns
+              names_sort = TRUE, #Sort alphabetically
+              values_from = ba_m, #Cells are BA
+              values_fn = sum, #Add BA together
+              values_fill = 0) -> edgeSpeciesBA #Fill NA with 0 and write it
 
-summary(merged)
+View(edgeSpeciesBA)
 
-#Let's play ----------------
-
-#This binning script was written by Dexter Locke for the SESYNC urban
-#woodlands pursuit. 
-
-#create a column with dbh classes
-
-plots_trees <-  df %>% mutate(dbh_class = santoku::chop(dbh_cm,  
-                                                breaks = seq(from = 2.54, #min DBH in dataset
-                                                to = 136, #Max DBH in dataset
-                                                by = 10)),         # 5 cm intervals
-         dbh_class = factor(dbh_class, ordered = TRUE), #Put them in order
-         ba_m = (dbh_cm^2)*0.00007854) #Add BA
-
-# how did the dbh class work out?
-table(plots_trees$dbh_class)
-round(100*prop.table(table(plots_trees$dbh_class)), 2)
-barplot(table(plots_trees$dbh_class), las=2) #DBH distribution by class. Boom.
-
-View(plots_trees)
-
-
-binned<-plots_trees %>% 
-  mutate(dbh_class = factor(dbh_class, ordered = TRUE,
-                            levels = c(
-                              '[2.54, 12.54)',
-                              '[12.54, 22.54)',
-                              '[22.54, 32.54)',
-                              '[32.54, 42.54)',
-                              '[42.54, 52.54)',
-                              '[52.54, 62.54)',
-                              '[62.54, 72.54)',
-                              '[72.54, 82.54)', 
-                              '[82.54, 92.54)',
-                              '[92.54, 102.5)', 
-                              '[102.54, 112.5)', 
-                              '[112.54, 122.5)', 
-                              '[122.54, 132.5)',
-                              '[132.54, 134.1'))) %>% 
-  drop_na(dbh_class) 
-
-View(binned)
-
-binned %>% 
-  select(PlotID,genus_species, trees_per_hectare) %>% 
-  pivot_wider(id_cols = c(data_source_plot_ID, year),
-              names_from = genus_species,
-              names_sort = TRUE,
-              values_from = trees_per_hectare,
-              values_fill = 0) %>% 
-  mutate(data_source_plot_ID_year = paste(data_source_plot_ID, year, sep = '_')) %>% 
-  select(data_source_plot_ID_year, everything(), -data_source_plot_ID, -year) -> trees_stem_density_ha_species_matrix
-
-st_write(binned, 'PlotTree.csv')
+write_csv(edgeSpeciesBA, 'edgeSpeciesBA.csv')
